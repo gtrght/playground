8c608129cea88d65610ecc759dd491628df39a1f
Vasily Vlasov <vasily.vlasov@gmail.com>
2018-03-17 17:15:10 -0700

    Simplified version of Akka Routing DSL sample

--- api-query/src/main/java/com/vvlasov/playground/rest/Launcher.java
+++ api-query/src/main/java/com/vvlasov/playground/rest/Launcher.java
@@ -2,12 +2,24 @@ package com.vvlasov.playground.rest;
 
 import com.vvlasov.playground.rest.akkahttp.AkkaApplicationServer;
 import com.vvlasov.playground.rest.vertx.VertxApplicationServer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
 
 /**
  * @author Vasily Vlasov
  */
+@Slf4j
 public class Launcher {
     public static void main(String[] args) {
-        new AkkaApplicationServer().start(args);
+        Application server;
+        if (Arrays.stream(args).anyMatch(val -> val.equalsIgnoreCase("akka"))) {
+            server = new AkkaApplicationServer();
+        } else if (Arrays.stream(args).anyMatch(val -> val.equalsIgnoreCase("vertx"))) {
+            server = new VertxApplicationServer();
+        } else {
+            server = new AkkaApplicationServer();
+        }
+        server.start(args);
     }
 }
--- api-query/src/main/java/com/vvlasov/playground/rest/akkahttp/AkkaApplicationServer.java
+++ api-query/src/main/java/com/vvlasov/playground/rest/akkahttp/AkkaApplicationServer.java
@@ -1,56 +1,51 @@
 package com.vvlasov.playground.rest.akkahttp;
 
 import akka.actor.ActorSystem;
-import akka.http.javadsl.model.*;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.HttpApp;
 import akka.http.javadsl.server.Route;
-import akka.http.scaladsl.Http;
-import akka.stream.javadsl.Source;
-import akka.util.ByteString;
 import com.vvlasov.playground.rest.Application;
 import lombok.SneakyThrows;
-import scala.concurrent.Future;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Vasily Vlasov
  */
+@Slf4j
 public class AkkaApplicationServer extends HttpApp implements Application {
+    private static final HttpResponse UNSUPPORTED_METHOD_RESPONSE = HttpResponse.create()
+            .withStatus(405)
+            .withEntity("Unsupported method!");
+
+    private static final HttpResponse NOT_FOUND_RESPONSE = HttpResponse.create()
+            .withStatus(404)
+            .withEntity("Unknown resource!");
+
+
     @Override
     @SneakyThrows
     public void start(String[] args) {
-        ActorSystem system = ActorSystem.create();
-        bindRoute("127.0.0.1",8080, system);
-        System.out.println("Type RETURN to exit");
-        System.in.read();
-        system.shutdown();
+        final ActorSystem system = ActorSystem.create("routing-dsl");
+        startServer("localhost", 8080, system);
     }
 
+    /**
+     * In the Java DSL, a Route can only consist of combinations of the built-in directives. A Route can not be instantiated directly.
+     * However, the built-in directives may be combined methods like:
+     *
+     * @return route for a server
+     * @see <a href="https://doc.akka.io/japi/akka-http/10.1.0/?akka/http/javadsl/server/Route.html">https://doc.akka.io/japi/akka-http/10.1.0/?akka/http/javadsl/server/Route.html</a>
+     * <p>
+     * Ok Boss! :)
+     */
     @Override
-    public Route createRoute() {
+    protected Route routes() {
+        Route route1 = pathEndOrSingleSlash(() -> complete("Directory"));
+        Route route2 = path("ping", () -> complete("pong!"));
         return route(
-                get(
-                        pathEndOrSingleSlash().route(complete("Directory")),
-                        path("ping").route(complete("PONG!")),
-//                        path("crash", route(get(()-> {
-//                            return complete("OK");
-//                        }))),
-                        path("stream").route(complete(streamingResponse()))
-                )
+                get(() -> route1),
+                get(() -> route2),
+                post(() -> complete(UNSUPPORTED_METHOD_RESPONSE))
         );
-    }
-
-
-    private HttpResponse streamingResponse() {
-        // TODO javadsl getting much more streamlined in 1.1 (released soon)
-        final Source<ByteString, Object> data = Source
-                .repeat("Hello!")
-                .take(5)
-                .map(ByteString::fromString)
-                .mapMaterializedValue(s -> null); // drop materialized value
-
-        final ContentType ct = ContentType.create(MediaTypes.TEXT_PLAIN);
-        final int len = 30; // TODO length not needed in 1.1
-        final HttpEntityDefault chunked = HttpEntities.create(ct, len, data);
-        return HttpResponse.create().withEntity(chunked);
     }
 }
